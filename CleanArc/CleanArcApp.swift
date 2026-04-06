3f76b2e698ef816a8b63503722a73a6b3dddd9cb
Youngmin322 <choyoungmin0322@gmail.com>
2026-04-06 16:18:31 +0900

    Initial Commit

---
+++ CleanArc/CleanArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArcApp.swift
+//  CleanArc
+//
+//  Created by Youngmin Cho on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CleanArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
