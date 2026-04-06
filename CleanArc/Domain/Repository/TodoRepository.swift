0424c387acd8b9e64d8bbef3a9b101c6ee86a235
Youngmin322 <choyoungmin0322@gmail.com>
2026-04-06 17:03:54 +0900

    feat: TodoRepository 프로토콜 정의

---
+++ CleanArc/Domain/Repository/TodoRepository.swift
@@ -0,0 +1,17 @@
+//
+//  TodoRepository.swift
+//  CleanArc
+//
+//  Created by Youngmin Cho on 4/6/26.
+//
+
+import Foundation
+
+protocol TodoRepository {
+    func todoInquiry() -> [TodoItem]
+    
+    func todoAdd(todo: TodoItem)
+    
+    func completed(todo: TodoItem)
+
+}
